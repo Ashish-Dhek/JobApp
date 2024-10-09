7decba18f71ebdf516abdb5f2173ab6e9b182212
Ashish <ashishdhek902gmail.com>
2024-10-09 18:22:22 +0530

    eight commit

--- Backend/app.js
+++ Backend/app.js
@@ -13,13 +13,13 @@ const app = express();
 dotenv.config({path: "./config/config.env"});
 
 
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+}));
 
-app.use(cors()) ;
+// app.use(cors()) ;
 
 
 app.use(cookieParser());
